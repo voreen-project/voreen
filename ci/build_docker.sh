7ca97889f7f20c04e045ae6fb059201fb6df817c
Dominik Drees <dominik.drees@wwu.de>
2019-03-21 16:13:13 +0100

    Add git to ci docker image

--- ci/build_docker.sh
+++ ci/build_docker.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+script=$0
+script_dir=$(dirname $script)
+
 mkdir empty
-docker build -f DockerfileBuildRunner -t "voreen/ci-ubuntu-16.04" empty
+docker build -f $script_dir/DockerfileBuildRunner -t "voreen/ci-ubuntu-16.04" empty
 rmdir empty
