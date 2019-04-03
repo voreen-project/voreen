964702c1bc0a524b7c6cdb66a3e4259a375e0ad6
Dominik Drees <dominik.drees@wwu.de>
2019-04-03 10:54:59 +0200

    Try to use ubuntu 18.04 as ci base

--- ci/build_docker.sh
+++ ci/build_docker.sh
@@ -3,5 +3,5 @@ script=$0
 script_dir=$(dirname $script)
 
 mkdir empty
-docker build -f $script_dir/DockerfileBuildRunner -t "voreen/ci-ubuntu-16.04" empty
+docker build -f $script_dir/DockerfileBuildRunner -t "voreen/ci-ubuntu-18.04" empty
 rmdir empty
