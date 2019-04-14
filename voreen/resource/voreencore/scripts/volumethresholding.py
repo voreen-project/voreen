0e9da37945f92cc28c5bc41596189bef52342722
Simon Leistikow <s_leis06@uni.muenster.de>
2019-04-14 12:13:46 +0200

    Update Dynamic Python examples

--- voreen/resource/voreencore/scripts/volumethresholding.py
+++ voreen/resource/voreencore/scripts/volumethresholding.py
@@ -2,15 +2,16 @@
 import voreen
 
 # Fetch port data.
-volume = voreen.getPortData("DynamicPythonProcessor", "VolumePort Inport #1")
+volume = voreen.getPortData("DynamicPythonProcessor", "VolumePort-in (1)")
 print ("Format: %s " % (volume.format))
 print ("Dimensions: (%i, %i, %i)" % (volume.dimX, volume.dimY, volume.dimZ))
 print ("Spacing: (%f, %f, %f)" % (volume.spacingX, volume.spacingY, volume.spacingZ))
 print ("Offset: (%f, %f, %f)" % (volume.offsetX, volume.offsetY, volume.offsetZ))
+print ("RealWorldMapping (Offset, Scale): (%f, %f)" % (volume.rwmOffset, volume.rwmScale))
 
 # Perform thresholding
 lowerThreshold = 0.0
-upperThreshold = 1.0
+upperThreshold = 0.7
 
 for i in range(0, len(volume.data)):
     if (volume.data[i] < lowerThreshold):
@@ -22,4 +23,4 @@ for i in range(0, len(volume.data)):
 volume.format = "float"
 
 # Set data to port.
-voreen.setPortData("DynamicPythonProcessor", "VolumePort Outport #2", volume);
+voreen.setPortData("DynamicPythonProcessor", "VolumePort-out (2)", volume);
