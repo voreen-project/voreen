0be19f984d947b8ec08a35f7ecc24d7f0daf6a1f
Dominik Drees <dominik.drees@wwu.de>
2019-05-03 11:29:28 +0200

    Add deploy build target to ci

---
+++ ci/build_deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+VRN_DIR=$(pwd)/voreen
+
+# clear build directory (also clears CMake cache)
+BUILD_DIR=$1
+rm -rf $BUILD_DIR
+mkdir $BUILD_DIR
+cd $BUILD_DIR
+
+INSTALL_DIR=$1
+
+# build configuration
+build_options=(
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_INSTALL_PREFIX=./voreen-install/
+    -DVRN_DEPLOYMENT=ON
+    -DVRN_ADD_INSTALL_TARGET=ON
+
+    #set custom module options here
+    -DVRN_MODULE_FFMPEG=ON
+    -DVRN_MODULE_HDF5=ON
+    -DVRN_MODULE_OPENCL=ON
+    -DVRN_MODULE_OPENMP=ON
+    -DVRN_MODULE_RANDOMWALKER=ON
+    -DVRN_MODULE_STEREOSCOPY=ON
+    -DVRN_MODULE_TIFF=ON
+
+)
+cmake "${build_options[@]}" $VRN_DIR
+
+N_CORES=$(nproc)
+# start build once again to make errors (if any) more readable
+nice make -j$N_CORES || make
