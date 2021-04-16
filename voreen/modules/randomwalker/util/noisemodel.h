4c74ccccc80bc66b5bab2678d7af2f5f61db21c7
d_dree02 <dominik.drees@wwu.de>
2021-04-16 20:48:45 +0200

    Speed and clean up rw ttest once more

--- voreen/modules/randomwalker/util/noisemodel.h
+++ voreen/modules/randomwalker/util/noisemodel.h
@@ -93,14 +93,6 @@ inline float square(float s) {
     return s*s;
 }
 
-//inline float gaussian_pdf_exp(float val, float mean, float variance) {
-//    return std::exp(-0.5f/variance * square(val-mean));
-//}
-//
-//inline float gaussian_pdf(float val, float mean, float variance) {
-//    return gaussian_pdf_exp(val, mean, variance)/std::sqrt(2.0f * 3.141592654f* variance);
-//}
-
 inline float variance_of(const float* begin, const float* end, float mean) {
 
     float sq_sum = 0.0f;
@@ -116,77 +108,36 @@ inline float variance_of(const float* begin, const float* end, float mean) {
     return variance;
 };
 
-#define VRN_FOR_EACH_VOXEL_INDEX(INDEX, BEGIN, END, SIZE, BODY) \
-    size_t vrn_fevi_z_slice_size = (SIZE).x*(SIZE).y;\
-    size_t vrn_fevi_z_begin = (BEGIN).z*vrn_fevi_z_slice_size;\
-    size_t vrn_fevi_z_end = (END).z*vrn_fevi_z_slice_size;\
-    for (size_t vrn_fevi_z = vrn_fevi_z_begin; vrn_fevi_z < vrn_fevi_z_end ; vrn_fevi_z += vrn_fevi_z_slice_size) {\
-        size_t vrn_fevi_y_begin = vrn_fevi_z + (BEGIN).y*(SIZE).x;\
-        size_t vrn_fevi_y_end = vrn_fevi_z + (END).y*(SIZE).x;\
-        for (size_t vrn_fevi_y = vrn_fevi_y_begin; vrn_fevi_y < vrn_fevi_y_end ; vrn_fevi_y += (SIZE).x){\
-            size_t vrn_fevi_x_begin = vrn_fevi_y + (BEGIN).x;\
-            size_t vrn_fevi_x_end = vrn_fevi_y + (END).x;\
-            for (size_t INDEX = vrn_fevi_x_begin; INDEX < vrn_fevi_x_end; ++INDEX){\
-                BODY\
-            }\
-        }\
-    }\
-
 inline void process_non_overlap_single(const float* image, tgt::ivec3 imagedim, tgt::ivec3 begin, tgt::ivec3 end, tgt::ivec3 overlap_begin, tgt::ivec3 overlap_end, float*& output_cur, float& sum) {
-#if 0
-    for(int dim = 2; dim >= 0; --dim) {
-        tgt::ivec3 part_begin = begin;
-        tgt::ivec3 part_end = end;
-        if(begin[dim] == overlap_begin[dim]) {
-            part_begin[dim] = overlap_end[dim];
-        }
-        if(end[dim] == overlap_end[dim]) {
-            part_end[dim] = overlap_begin[dim];
-        }
+    size_t sliceSize = imagedim.y * imagedim.x;
+    size_t lineSize = imagedim.x;
+    for (size_t z = begin.z; z < static_cast<int>(end.z); ++z) {
 
-        VRN_FOR_EACH_VOXEL_INDEX(l, part_begin, part_end, imagedim, {
-            float val = image[l];
-            *output_cur = val;
-            ++output_cur;
-            sum += val;
-        })
+        size_t zIndex = sliceSize * z;
+        bool zIn = overlap_begin.z <= z && z < overlap_end.z;
 
-        begin[dim] = overlap_begin[dim];
-        end[dim] = overlap_end[dim];
-    }
-#else
-        {
-            size_t sliceSize = imagedim.y * imagedim.x;
-            size_t lineSize = imagedim.x;
-            for (size_t z = begin.z; z < static_cast<int>(end.z); ++z) {
+        for (size_t y = begin.y; y < static_cast<int>(end.y); ++y) {
 
-                size_t zIndex = sliceSize * z;
-                bool zIn = overlap_begin.z <= z && z < overlap_end.z;
+            size_t yIndex = zIndex + lineSize * y;
+            bool yIn = zIn && overlap_begin.y <= y && y < overlap_end.y;
 
-                for (size_t y = begin.y; y < static_cast<int>(end.y); ++y) {
+            size_t iBegin = yIndex + begin.x;
+            size_t iEnd = yIndex + end.x;
 
-                    size_t yIndex = zIndex + lineSize * y;
-                    bool yIn = zIn && overlap_begin.y <= y && y < overlap_end.y;
+            size_t overlap_i_begin = yIndex + overlap_begin.x;
+            size_t overlap_i_end = yIndex + overlap_end.x;
+            for (size_t i = iBegin; i < iEnd; ++i) {
+                bool xIn = yIn && overlap_i_begin <= i && i < overlap_i_end;
 
-                    size_t iBegin = yIndex + begin.x;
-                    size_t iEnd = yIndex + end.x;
-
-                    size_t overlap_i_begin = yIndex + overlap_begin.x;
-                    size_t overlap_i_end = yIndex + overlap_end.x;
-                    for (size_t i = iBegin; i < iEnd; ++i) {
-                        bool xIn = yIn && overlap_i_begin <= i && i < overlap_i_end;
-
-                        if(!xIn) {
-                            float val = image[i];
-                            *output_cur = val;
-                            ++output_cur;
-                            sum += val;
-                        }
-                    }
+                if(!xIn) {
+                    float val = image[i];
+                    *output_cur = val;
+                    ++output_cur;
+                    sum += val;
                 }
             }
         }
-#endif
+    }
 }
 
 // Parameter estimation according to
@@ -202,11 +153,12 @@ struct RWNoiseModelTTest {
     };
 
     VolumeAtomic<float> image;
-    std::vector<MeanMulAdd> mean_mul_add;
+    VolumeAtomic<tgt::ivec3> best_centers;
 
     RWNoiseModelTTest(RWNoiseModelTTest&&) = default;
 
     static RWNoiseModelTTest prepare(const VolumeAtomic<float>& vol, RealWorldMapping rwm) {
+        tgt::ivec3 dim = vol.getDimensions();
 
         VolumeAtomic<float> image = vol.copy();
         VolumeAtomic<float> mean = meanFilter<filter_extent>(image);
@@ -225,9 +177,40 @@ struct RWNoiseModelTTest {
             mean_mul_add.push_back(mma);
         }
 
+        VolumeAtomic<tgt::ivec3> best_centers(dim);
+
+        VRN_FOR_EACH_VOXEL(p, tgt::ivec3::zero, dim) {
+            float f = image.voxel(p);
+
+            const tgt::ivec3 extent(filter_extent);
+
+            tgt::ivec3 begin = tgt::max(tgt::ivec3::zero, p - extent);
+            tgt::ivec3 end = tgt::min(dim, p + tgt::ivec3::one + extent);
+
+            float min = std::numeric_limits<float>::infinity();
+            size_t argmin_index = 0;
+
+            VRN_FOR_EACH_VOXEL_INDEX(l, begin, end, dim, {
+                //float pdf_val = gaussian_pdf(f, mean.voxel(n), variance.voxel(n));
+                // Instead of evaluating and maximizing the gaussian pdf (which
+                // is expensive) we minimize the negative log instead.
+                const auto& mma = mean_mul_add[l];
+                float val = square(f-mma.mean) * mma.mul + mma.add;
+                tgtAssert(std::isfinite(val) && !std::isnan(val), "invalid val");
+                if(min > val) {
+                    min = val;
+                    argmin_index = l;
+                }
+            })
+
+            tgt::ivec3 argmin = linearCoordToCubic(argmin_index, dim);
+            tgtAssert(tgt::max(tgt::abs(p - argmin)) <= filter_extent, "invalid pos");
+            best_centers.voxel(p) = argmin;
+        }
+
         return RWNoiseModelTTest {
             std::move(image),
-            std::move(mean_mul_add),
+            std::move(best_centers),
         };
     }
     static RWNoiseModelTTest prepare(const VolumeRAM& vol, RealWorldMapping rwm) {
@@ -259,36 +242,8 @@ struct RWNoiseModelTTest {
 
         const tgt::ivec3 extent(filter_extent);
 
-        auto best_neighborhood = [&] (tgt::ivec3 p) -> tgt::ivec3 {
-            float f = image.voxel(p);
-
-            tgt::ivec3 begin = tgt::max(tgt::ivec3::zero, p - extent);
-            tgt::ivec3 end = tgt::min(dim, p + tgt::ivec3::one + extent);
-
-            float min = std::numeric_limits<float>::infinity();
-            size_t argmin_index = 0;
-
-            VRN_FOR_EACH_VOXEL_INDEX(l, begin, end, dim, {
-                //float pdf_val = gaussian_pdf(f, mean.voxel(n), variance.voxel(n));
-                // Instead of evaluating and maximizing the gaussian pdf (which
-                // is expensive) we minimize the log instead.
-                const auto& mma = mean_mul_add[l];
-                float val = square(f-mma.mean) * mma.mul + mma.add;
-                tgtAssert(std::isfinite(val) && !std::isnan(val), "invalid val");
-                if(min > val) {
-                    min = val;
-                    argmin_index = l;
-                }
-            })
-
-            tgt::ivec3 argmin = linearCoordToCubic(argmin_index, dim);
-            tgtAssert(tgt::max(tgt::abs(p - argmin)) <= filter_extent, "invalid pos");
-
-            return argmin;
-        };
-
-        auto best_center1 = best_neighborhood(p1);
-        auto best_center2 = best_neighborhood(p2);
+        auto best_center1 = best_centers.voxel(p1);
+        auto best_center2 = best_centers.voxel(p2);
 
         tgt::ivec3 begin1 = tgt::max(tgt::ivec3::zero, best_center1 - extent);
         tgt::ivec3 end1 = tgt::min(dim, best_center1 + tgt::ivec3::one + extent);
@@ -319,6 +274,14 @@ struct RWNoiseModelTTest {
         auto* o_begin = overlap.data();
         auto* o_cur = o_begin;
 
+        // In most cases (except when at the border of the volume) both
+        // neighborhoods are placed with rotational symmetry at opposite sides
+        // of the overlap region. In this case we can sample from both regions
+        // simultaneously with some index trickery: We iterate normally over
+        // region 1, but do so in reverse for region 2. Then, difference
+        // between the index of the first voxel of region 1 and the current
+        // once is the same as the difference between the current voxel in
+        // region 2 and the last voxel of the region (end-1).
         if(end1-begin1 == end2-begin2) {
             size_t b1index = cubicCoordToLinear(begin1, dim);
             size_t e2index = cubicCoordToLinear(end2-tgt::ivec3::one, dim);
@@ -327,41 +290,51 @@ struct RWNoiseModelTTest {
             {
                 size_t zBegin = begin1.z*sliceSize;
                 size_t zEnd = end1.z*sliceSize;
-                for (size_t z = begin1.z; z < static_cast<int>(end1.z); ++z) {
+                for (size_t z = begin1.z; z < end1.z; ++z) {
 
                     size_t zIndex = sliceSize * z;
                     bool zIn = overlap_begin.z <= z && z < overlap_end.z;
 
-                    for (size_t y = begin1.y; y < static_cast<int>(end1.y); ++y) {
+                    for (size_t y = begin1.y; y < end1.y; ++y) {
 
                         size_t yIndex = zIndex + lineSize * y;
                         bool yIn = zIn && overlap_begin.y <= y && y < overlap_end.y;
 
-                        for (size_t x = begin1.x; x < static_cast<int>(end1.x); ++x) {
-                            bool xIn = yIn && overlap_begin.x <= x && x < overlap_end.x;
+                        size_t non_overlap_begin;
+                        size_t non_overlap_end;
+                        if(yIn) {
+                            for (size_t x = overlap_begin.x; x < overlap_end.x; ++x) {
+                                size_t i = yIndex + x;
+                                float val = image.voxel(i);
 
-                            size_t i = yIndex + x;
-                            float val1 = image.voxel(i);
-
-                            if(xIn) {
                                 tgt::ivec3 n(x,y,z);
                                 int along_axis = tgt::dot(n, from_b1_to_b2);
                                 along_axis = (along_axis << 16) + neighborhood_counter;
-                                *o_cur = std::make_pair(val1,along_axis);
+                                *o_cur = std::make_pair(val,along_axis);
                                 ++o_cur;
                                 ++neighborhood_counter;
-                            } else {
-                                *n1_cur = val1;
-                                ++n1_cur;
-                                sum1 += val1;
-
-                                size_t i2 = i2base - i;
-                                tgtAssert(i2 < tgt::hmul(dim) ,"invalid index");
-                                float val2 = image.voxel(i2);
-                                *n2_cur = val2;
-                                ++n2_cur;
-                                sum2 += val2;
                             }
+                            bool overlap_at_begin = overlap_begin.x == begin1.x;
+                            non_overlap_begin = overlap_at_begin ? overlap_end.x : begin1.x;
+                            non_overlap_end = overlap_at_begin ? end1.x : overlap_begin.x;
+                        } else {
+                            non_overlap_begin = begin1.x;
+                            non_overlap_end = end1.x;
+                        }
+
+                        for (size_t x = non_overlap_begin; x < non_overlap_end; ++x) {
+                            size_t i = yIndex + x;
+                            float val1 = image.voxel(i);
+                            *n1_cur = val1;
+                            ++n1_cur;
+                            sum1 += val1;
+
+                            size_t i2 = i2base - i;
+                            tgtAssert(i2 < tgt::hmul(dim) ,"invalid index");
+                            float val2 = image.voxel(i2);
+                            *n2_cur = val2;
+                            ++n2_cur;
+                            sum2 += val2;
                         }
                     }
                 }
@@ -370,15 +343,15 @@ struct RWNoiseModelTTest {
             process_non_overlap_single(image.voxel(), dim, begin1, end1, overlap_begin, overlap_end, n1_cur, sum1);
             process_non_overlap_single(image.voxel(), dim, begin2, end2, overlap_begin, overlap_end, n2_cur, sum2);
             size_t neighborhood_counter = 0;
-            for (size_t z = overlap_begin.z; z < static_cast<int>(overlap_end.z); ++z) {
+            for (size_t z = overlap_begin.z; z < overlap_end.z; ++z) {
 
                 size_t zIndex = sliceSize * z;
 
-                for (size_t y = overlap_begin.y; y < static_cast<int>(overlap_end.y); ++y) {
+                for (size_t y = overlap_begin.y; y < overlap_end.y; ++y) {
 
                     size_t yIndex = zIndex + lineSize * y;
 
-                    for (size_t x = overlap_begin.x; x < static_cast<int>(overlap_end.x); ++x) {
+                    for (size_t x = overlap_begin.x; x < overlap_end.x; ++x) {
                         size_t i = yIndex + x;
                         float val = image.voxel(i);
 
